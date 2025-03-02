f961fca17d803f9afbeb116aea88e7467a404038
Lutful Bari Musa <lutfulbarimusa@gmail.com>
2025-03-02 20:44:39 +0600

    this is Enum

---
+++ DatatypeVariable/All_Enum.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatatypeVariable
+{
+    class All_Enum
+    {
+        public enum Country:int
+        {
+            bd = 1,
+            usa = 2,
+            Uk = 3,
+        }
+        public enum Countre:int
+        {
+            Dhaka = 1,
+            mirpur = 2,
+            monipurar = 3,
+
+
+        }
+    }
+}
--- DatatypeVariable/Program.cs
+++ DatatypeVariable/Program.cs
@@ -31,17 +31,20 @@ namespace DatatypeVariable
             //Console.WriteLine(value);
 
             //bool value = false;
-            //Console.WriteLine(value);
+            ////Console.WriteLine(value);
 
-            double d = 10.50000213213;
+            //double d = 10.50000213213;
 
-            float f = 10.5f;
+            //float f = 10.5f;
 
-            decimal dec = 222.5m;
+            //decimal dec = 222.5m;
 
-            Console.WriteLine(d);
-            Console.WriteLine(f);
-            Console.WriteLine(dec);
+            //Console.WriteLine(d);
+            //Console.WriteLine(f);
+            //Console.WriteLine(dec);
+
+            Console.WriteLine("hello Enum:  " + All_Enum.Country.bd);
+            Console.WriteLine("hello Enum:  " + All_Enum.Countre.Dhaka);
 
 
 
