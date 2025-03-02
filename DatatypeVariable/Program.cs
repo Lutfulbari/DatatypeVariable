e2b4e1bf3af332471410a9b6a4285cf68c14017e
Lutful Bari Musa <lutfulbarimusa@gmail.com>
2025-03-02 20:15:45 +0600

    Add project files.

---
+++ DatatypeVariable/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatatypeVariable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int a = 10;
+            //long b = 20;
+            //long c = a + b;
+            //Console.WriteLine("a+b= " + c);
+            //Console.ReadLine();
+
+
+            //string Name = "Lutful";
+
+            //Console.WriteLine(Name);
+
+            //char value = 'a';
+            //Console.WriteLine(value);
+            //Console.WriteLine((int)value);
+
+            //bool data type
+
+            //bool value = true;
+            //Console.WriteLine(value);
+
+            //bool value = false;
+            //Console.WriteLine(value);
+
+            double d = 10.50000213213;
+
+            float f = 10.5f;
+
+            decimal dec = 222.5m;
+
+            Console.WriteLine(d);
+            Console.WriteLine(f);
+            Console.WriteLine(dec);
+
+
+
+        }
+    }
+}
